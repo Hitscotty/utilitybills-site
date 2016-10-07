d57f51504cd28755b2778beb3f080b91af891c77
Jonathan <Jportorreal77@gmail.com>
2016-10-06 22:17:10 -0400

    finished mvp of desktop frontend, started working on the mobile site

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,6 @@
 
 $(document).ready(function() {
-
  $('#footer67').click(function () {
   window.location.href="index.html";
  });
-
- $('pi').hover(function(){
- $('section').css("background-color", "red", "z-index", "1000");
-},
- function(){
-  $('html').css("background-color", "none");
- });
-
  });
